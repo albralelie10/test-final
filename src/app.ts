5d68c786eeb5430284206004d2752c3c33cac950
polo <polo@contoso.com>
2023-09-13 03:16:22 -0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@ import routerUser from "./api/user/user.router"
 import express from "express"
 import { Response,Request } from "express";
 import * as middlewares from './middlewares';
+import cors from "cors"
 
 
 const app=express()
+
+app.use(cors({
+    origin:"*",
+    methods:["GET","DELETE","PUT","POST","OPTIONS"]
+}))
+app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use("/users",routerUser);
 app.use(middlewares.errorHandler)
@@ -13,6 +20,5 @@ app.use(middlewares.errorHandler)
 app.get("/",(req:Request,res:Response)=>{
     return res.json({msg:"START HOME PAGE"})
 })
-
 
 export default app;
